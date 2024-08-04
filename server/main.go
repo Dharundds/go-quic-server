92a05e1cb38d117732b9ac3d0b1c44c059dc378c
C Dharun <dharundds@gmail.com>
2024-08-04 14:39:45 +0530

    UPDATE: MUX with 0RTT

--- server/main.go
+++ server/main.go
@@ -4,40 +4,45 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/Dharundds/go-quic-server/helpers"
+	"github.com/quic-go/quic-go"
 	"github.com/quic-go/quic-go/http3"
 )
 
 func main(){
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/",homeHandler)
-	mux.HandleFunc("/other",otherHandler)
-	mux.HandleFunc("/404", notFoundHandler)
+	mux.HandleFunc("GET /",homeHandler)
+	mux.HandleFunc("POST /other",otherHandler)
+	mux.HandleFunc("GET /404", notFoundHandler)
 	// mux.HandleFunc("/", notFoundHandler)
 
 	log.Printf("Http/3 Server listening on %s", "localhost:5000")
-	err := http3.ListenAndServeQUIC(
-		":5000",
-		"certificate.crt",
-		"key.pem",
-		mux,
-	)
-
-
-	if err != nil{
-		log.Fatalf("Error in serving http3 server %v",err)
+	server := http3.Server{
+		Addr: "localhost:5000",
+		Handler: mux,
+		TLSConfig: helpers.GenerateTLSConfig(),
+		QUICConfig: &quic.Config{Allow0RTT: true},
 	}
+	// err := http3.ListenAndServeQUIC(
+	// 	":5000",
+	// 	"certificate.crt",
+	// 	"key.pem",
+	// 	mux,
+	// )
+	log.Fatal(server.ListenAndServeTLS("",""))
+
+	// if err != nil{
+	// 	log.Fatalf("Error in serving http3 server %v",err)
+	// }
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request){
-	if r.Method == http.MethodGet{
-		log.Printf("Got GET request %v",r.Proto)
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("GET request received"))
-	} else{
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		w.Write([]byte("/ only accepts GET"))
-	}
+	
+	log.Printf("Got GET request %v %v",r.Proto, r.TLS.HandshakeComplete)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("GET request received"))
+	
 }
 
 func otherHandler(w http.ResponseWriter, r *http.Request){
@@ -62,3 +67,4 @@ func notFoundHandler(w http.ResponseWriter, r *http.Request) {
     w.WriteHeader(http.StatusNotFound) // Set status code to 404 Not Found
     w.Write([]byte("404 - Page not found"))
 }
+
